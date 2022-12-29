96903f89b09faeaaa0106f990619e88cfb014a61
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2022-12-29 17:26:07 -0300

    initial commit 🍺

--- SwUi_NavigationStack/ContentView.swift
+++ SwUi_NavigationStack/ContentView.swift
@@ -4,18 +4,37 @@
 //
 //  Created by Marcelo Sampaio on 29/12/22.
 //
-
+/*
+ https://www.devtechie.com/community/public/posts/211436-how-to-change-navigationstack-background-color-in-swiftui
+ */
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = UIColor.systemYellow
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Text("Build Photo Gallery in SwiftUI Course at DevTechie.com")
+                    .font(.largeTitle)
+                    .foregroundColor(.init(uiColor: .darkText))
+                    .padding()
+                
+                Spacer()
+                
+            }
+            .navigationTitle("DevTechie.com")
+            .navigationBarTitleDisplayMode(.inline)
+
         }
-        .padding()
     }
 }
 
