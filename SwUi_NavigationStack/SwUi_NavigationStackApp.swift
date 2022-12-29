9586a21299d7053cc61406333d013b805db86262
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2022-12-29 16:58:30 -0300

    Initial Commit

---
+++ SwUi_NavigationStack/SwUi_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_NavigationStackApp.swift
+//  SwUi_NavigationStack
+//
+//  Created by Marcelo Sampaio on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
